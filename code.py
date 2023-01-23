9e6571f11fa44f84b48db476daa0e4e9108a656c
liulin0x3c <liulin0x3c@foxmail.com>
2023-01-23 15:52:06 +0800

    add annotation

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+# 该版本的代码发布到了我的github上
+# url: https://github.com/liulin0x3c/CNN-MNIST
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms  # 数据的原始处理
@@ -17,17 +19,26 @@ test_loader = DataLoader(dataset=test_dataset, shuffle=False, batch_size=batch_s
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+        # 第一个卷积层
         self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5)
+        # 第二个卷积层
         self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5)
+        # 池化层
         self.pooling = torch.nn.MaxPool2d(2)
+        # 分类用的线性层
         self.fc = torch.nn.Linear(320, 10)
 
+    # 下面就是计算的过程
     def forward(self, x):
         # Flatten data from (n, 1, 28, 28) to (n, 784)
-        batch_size = x.size(0)
+        batch_size = x.size(0)  # 这里面的0是x大小第1个参数，自动获取batch大小
+        # 输入x经过一个卷积层，之后经历一个池化层，最后用relu做激活
         x = F.relu(self.pooling(self.conv1(x)))
         x = F.relu(self.pooling(self.conv2(x)))
+        # 为了给我们最后一个全连接的线性层用
+        # 我们要把一个二维的图片（实际上这里已经是处理过的）20x4x4张量变成一维的
         x = x.view(batch_size, -1)  # flatten
+        # 经过线性层，确定他是0~9每一个数的概率
         x = self.fc(x)
         return x
 
